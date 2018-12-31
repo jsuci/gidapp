c82d52937215c322d7174101fd1d339f823e9260
jsuci <jsuci.jsuci@gmail.com>
2018-12-31 21:26:04 +0800

    fix file path argument for android.intent

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@ echo "Fetching SW3 and STL results."
 cd ~/gidapp/swertres
 python sw3_results_v1.py
 python sw3_results_v2.py
-am start -a android.intent.action.VIEW -d file://~/sdcard/gidapp/swertres/results_v2.txt -t text/plain --activity-clear-task
 cd ~/gidapp/stl
 python stl_results_v1.py
 python stl_results_v2.py
 cd ~
+am start -a android.intent.action.VIEW -d file://~/sdcard/Download/gidapp/swertres/results_v2.txt -t text/plain --activity-clear-task
 echo "Exiting program in 30 seconds."
 sleep 30s
 killall com.termux
