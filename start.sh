c6bb0b3fdc7e81e9b236e8a16e9a23e5be355c23
jsuci <jsuci.jsuci@gmail.com>
2019-01-01 17:02:55 +0800

    add new entries to shell

--- start.sh
+++ start.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/sh
 
 clear
-echo "Fetching SW3 and STL results."
+echo "Fetching SW3 results."
 cd /sdcard/Download/gidapp/swertres
 python sw3_results_v1.py
 python sw3_results_v2.py
+echo
+echo "Fetching STL results."
 cd /sdcard/Download/gidapp/stl
 python stl_results_v1.py
 python stl_results_v2.py
+echo
+echo
+echo "Process sum for SW3."
+cd /sdcard/Download/gidapp/swertres
+python sum_digits_v2.1.py
+echo
+echo "Process sum for STL."
+cd /sdcard/Download/gidapp/stl
+python sum_digits_v2.1.py
 cd ~
-am start -a android.intent.action.VIEW -d file://~/sdcard/Download/gidapp/swertres/results_v2.txt -t text/plain --activity-clear-task
+#am start -a android.intent.action.VIEW -d file://~/sdcard/Download/gidapp/swertres/results_v2.txt -t text/plain --activity-clear-task
 echo "Exiting program in 30 seconds."
 sleep 30s
 killall com.termux
