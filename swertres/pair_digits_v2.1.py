a477a3d695f42b8a666cab8d847141e5e3f979ae
jsuci <jsuci.jsuci@gmail.com>
2019-01-07 19:01:54 +0800

    remove strict mode on pair_digits_v2.1.py

--- swertres/pair_digits_v2.1.py
+++ swertres/pair_digits_v2.1.py
@@ -109,8 +109,8 @@ def get_output(to_skip):
                     if seq_digit:
                         if is_total_seq:
                             print("result_gap: {}\ntime: {}\ncommon_digit: {}\nis_total_seq: {}\nprev_results: {}\nseq_digit: {}\n\n".format(outer_count, k, common_digit, is_total_seq, v, seq_digit))
-                        # else:
-                        #     print("result_gap: {}\ntime: {}\ncommon_digit: {}\nis_total_seq: {}\nprev_results: {}\nseq_digit: {}\n\n".format(outer_count, k, common_digit, is_total_seq, v, seq_digit))
+                        else:
+                            print("result_gap: {}\ntime: {}\ncommon_digit: {}\nis_total_seq: {}\nprev_results: {}\nseq_digit: {}\n\n".format(outer_count, k, common_digit, is_total_seq, v, seq_digit))
 
 
 
