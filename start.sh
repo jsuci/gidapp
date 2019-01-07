9a917ef3f2bcd6025a55c2337b424af16d73b13e
jsuci <jsuci.jsuci@gmail.com>
2019-01-07 13:44:40 +0800

    update formatting of shell script

--- start.sh
+++ start.sh
@@ -11,6 +11,7 @@ cd /sdcard/Download/gidapp/stl
 python stl_results_v1.py
 python stl_results_v2.py
 echo
+echo "#####################"
 echo
 echo "Process sum for SW3."
 cd /sdcard/Download/gidapp/swertres
@@ -20,15 +21,18 @@ echo "Process sum for STL."
 cd /sdcard/Download/gidapp/stl
 python sum_digits_v2.1.py
 echo
+echo "#####################"
 echo
 echo "Process pair for SW3"
 cd /sdcard/Download/gidapp/swertres
 python pair_digits_v2.1.py
 echo
+echo
 echo "Process pair for STL"
 cd /sdcard/Download/gidapp/stl
 python pair_digits_v2.1.py
 echo
+echo "#####################"
 echo
 echo "Current results for SW3"
 cd /sdcard/Download/gidapp/swertres
@@ -39,6 +43,7 @@ cd /sdcard/Download/gidapp/stl
 python current_results.py
 cd ~
 echo
+echo "#####################"
 echo
 #am start -a android.intent.action.VIEW -d file://~/sdcard/Download/gidapp/swertres/results_v2.txt -t text/plain --activity-clear-task
 read -p "Do you want to exit? (Y/y): " -n 1 -r
