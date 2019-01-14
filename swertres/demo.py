24569d6e460c45587fbdcfb02b7ebf615fb8b509
jsuci <jsuci.jsuci@gmail.com>
2019-01-14 17:14:59 +0800

    update results and other misc files

--- swertres/demo.py
+++ swertres/demo.py
@@ -1,123 +1,91 @@
-import re
+# from itertools import islice
+
+
+# def is_sequence(list_of_digits):
+#     list_of_digits.sort()
+#     start = list_of_digits[0]
+#     first_digit = list_of_digits[0]
+#     last_digit = list_of_digits[-1]
+#     diff_not_one = []
+
+#     print(list_of_digits)
+
+#     for digit in islice(list_of_digits, 1, None):
+#         print(start, digit, abs(start - digit))
+#         if abs(start - digit) != 1:
+#             diff_not_one.append(abs(start - digit))
+
+#         start = digit
+
+#     diff_not_one.sort()
+#     len_diff_not_one = len(diff_not_one)
+
+
+#     print(diff_not_one)
+
+#     if not len_diff_not_one:
+#         print(1)
+#     else:
+#         if first_digit == 0 and last_digit == 9:
+#             if len_diff_not_one == 1:
+#                 print(1)
+#             elif len_diff_not_one == 2 and diff_not_one[0] == 2:
+#                 print(2)
+#             else:
+#                 print(0)
+#         elif first_digit == 0 and last_digit == 8:
+#             if len_diff_not_one == 1:
+#                 print(2)
+#             else:
+#                 print(0)
+#         else:
+#             if len_diff_not_one == 1 and diff_not_one[0] == 2:
+#                 print(2)
+
+# is_sequence([1, 2, 3, 6, 0, 8, 9])
+
 from itertools import islice
 
 
-def get_gap_results():
-    """Gathers all gap results and returns a list of tuple
-    containing the gap_value and time, results dictionary
-    ex. [(2, {'11am': ['952', '517', '591'], '4pm': ['371',
-    '976', '433'], '9pm': ['019', '928', '653']})...]
-    """
+def is_sequence(list_of_digits):
+    list_of_digits.sort()
+    start = list_of_digits[0]
+    first_digit = list_of_digits[0]
+    last_digit = list_of_digits[-1]
+    diff_not_one = []
 
-    gap_results_list = []
+    print(list_of_digits)
 
-    for gap_value in range(2, 50):
-        with open("results_v2.txt", "r") as fo:
-            last_entry = fo.readline().strip().split(" ")[-1]
-            reversed_entries = list(islice(fo, 1, None))[::-1]
-            to_skip = 0 if last_entry == "2" else 1
-            step_value = gap_value + to_skip
-            number_of_matches = 0
-            gap_results = {"11am": [], "4pm": [], "9pm": []}
-
-            for line_count, line_entry in enumerate(reversed_entries):
-                date_results_list = re.split(r"\s{10}", line_entry.strip())
-
-                # Set number of matches here
-                if line_count == step_value and number_of_matches != 3:
-                    gap_results["11am"].append(date_results_list[1])
-                    gap_results["4pm"].append(date_results_list[2])
-                    gap_results["9pm"].append(date_results_list[3])
-
-                    number_of_matches += 1
-                    step_value += gap_value + 1
-
-            gap_results_list.append((gap_value, gap_results))
-
-    return gap_results_list
-
-
-def is_sequence(list_digits):
-    """Given a list of unsorted integers ex. [4, 8, 3] check
-    if all the values inside has a difference of one.
-    """
-    sorted_digits = sorted(list_digits)
-    start = sorted_digits[0]
-    result = True
-
-    for digit in islice(sorted_digits, 1, None):
-
-        """Catch case where in [0, 8, 9] and [0, 1, 9]
-        are considered sequence. Note that digits are in
-        length of 3
-        """
-        if not (
-            (start == 0 and abs(start - digit) == 8) or
-            (start == 1 and abs(start - digit) == 8) or
-                (abs(start - digit) == 1)):
-
-            result = False
+    for digit in islice(list_of_digits, 1, None):
+        print(start, digit, abs(start - digit))
+        if abs(start - digit) != 1:
+            diff_not_one.append(abs(start - digit))
 
         start = digit
 
-    return result
+    diff_not_one.sort()
+    len_diff_not_one = len(diff_not_one)
 
+    print(diff_not_one)
 
-def is_sync(results):
-    """Given a list of results ex. ['358', '468', '827']
-    check if is in sync or not. If yes then output left,
-    right digits, pair_common_digit, common_digit
-    ex. [3, 4, 2] [5, 6, 7] [('38', '58'), ('48', '68'),
-    ('28', '78')] 8
-    """
-    for i in range(10):
-        common_digit = str(i)
-        has_common_digit = True
-        left_digit = []
-        right_digit = []
-        pair_common_digit = []
-
-        for result in results:
-            if common_digit not in result:
-                has_common_digit = False
+    if not len_diff_not_one:
+        print(1)
+    else:
+        if first_digit == 0 and last_digit == 9:
+            if len_diff_not_one == 1:
+                print(1)
+            elif len_diff_not_one == 2 and diff_not_one[0] == 2:
+                print(2)
             else:
-                pairs = result.replace(common_digit, "", 1)
-                left_pair = "".join([pairs[0], common_digit])
-                right_pair = "".join([pairs[1], common_digit])
-
-                left_digit.append(int(pairs[0]))
-                right_digit.append(int(pairs[1]))
-                pair_common_digit.append((left_pair, right_pair))
-
-        if (has_common_digit and
-                (is_sequence(left_digit) and is_sequence(right_digit))):
-            # print(left_digit, right_digit, pair_common_digit, common_digit)
-
-            return left_digit, right_digit, pair_common_digit, common_digit
-        # if has_common_digit and is_sequence(
-        #         left_digit) and is_sequence(right_digit):
-        #     return left_digit, right_digit, pair_common_digit, common_digit
+                print(0)
+        elif first_digit == 0 and last_digit == 8:
+            if len_diff_not_one == 1:
+                print(2)
+            else:
+                print(0)
+        else:
+            if len_diff_not_one == 1 and diff_not_one[0] == 2:
+                print(2)
 
 
-def main():
-    for item in get_gap_results():
-        gap_value, gap_results = item
-        for time, results in gap_results.items():
-            if is_sync(results):
-                left, right, pairs, common = is_sync(results)
-
-                print("gap: {}\ntime: {}\nresults: {}\n".format(
-                    gap_value, time, results), end="")
-                print("common_digit: {}\nleft: {}\n".format(
-                    common, left), end="")
-                print("right: {}\npairs (curr -> prev):".format(
-                    right))
-
-                for pair in pairs:
-                    print(pair)
-
-                print("\n\n")
-
-
-if __name__ == "__main__":
-    main()
+is_sequence([1, 2, 3, 6, 0, 8, 9])
