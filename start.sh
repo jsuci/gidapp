2d129e0af3aa8acd46a7ec7a4af48ea46e09d4cf
jsuci <jsuci.jsuci@gmail.com>
2019-01-14 17:21:50 +0800

    fix shell script format output

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ clear
 echo "PROCESSING SW3:"
 cd /sdcard/Download/gidapp/swertres
 echo
+echo "Current Results:"
+python current_results.py
+echo
 echo "Fetching Results:"
 python sw3_results_v1.py
 python sw3_results_v2.py
@@ -11,23 +14,23 @@ echo
 # echo "SUM Digits Pattern V2.1:"
 # python sum_digits_v2.1.py
 # echo
-# echo "SYNC Digits Pattern V2.1:"
-# python sync_digits_v2.1.py
-# echo
+echo "SYNC Digits Pattern V2.1:"
+python sync_digits_v2.1.py
+echo
 # echo "SYNC Digits Pattern V1.1:"
 # python sync_digits_v1.1.py
 # echo
 echo "MISSING Digits Pattern V2.1:"
 python missing_digits_v2.1.py
 echo
-echo "Current Results:"
-python current_results.py
-echo
 echo
 echo
 echo
 echo "PROCESSING STL:"
 cd /sdcard/Download/gidapp/stl
+echo
+echo "Current Results:"
+python current_results.py
 echo
 echo "Fetching results:"
 python stl_results_v1.py
@@ -36,17 +39,14 @@ echo
 # echo "SUM Digits Pattern V2.1:"
 # python sum_digits_v2.1.py
 # echo
-# echo "SYNC Digits Pattern V2.1:"
-# python sync_digits_v2.1.py
-# echo
+echo "SYNC Digits Pattern V2.1:"
+python sync_digits_v2.1.py
+echo
 # echo "SYNC Digits Pattern V1.1:"
 # python sync_digits_v1.1.py
 # echo
 echo "MISSING Digits Pattern V2.1:"
 python missing_digits_v2.1.py
-echo
-echo "Current Results:"
-python current_results.py
 echo
 echo
 echo
@@ -54,7 +54,7 @@ echo
 #am start -a android.intent.action.VIEW -d file://~/sdcard/Download/gidapp/swertres/results_v2.txt -t text/plain --activity-clear-task
 read -p "Do you want to exit? (Y/y): " -n 1 -r
 echo
-if [[ $REPLY =~ ^[Yy]$ ]]
+if [[ $REPLY =~ ^[ ]$ ]]
 then
 	echo
 	echo "Exiting program in 5 seconds."
