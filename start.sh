cd9e6310a407c4d1b5e8c16893eeec7a7c65463a
jsuci <jsuci.jsuci@gmail.com>
2019-01-28 07:41:46 +0800

    change the order of commands

--- start.sh
+++ start.sh
@@ -4,6 +4,12 @@ clear
 echo "PROCESSING SW3:"
 cd /sdcard/Download/gidapp/swertres
 echo
+echo "SYNC Digits Pattern V2.1:"
+python sync_digits_v2.1.py
+echo
+echo "SYNC Digits Pattern V2.2:"
+python sync_digits_v2.2.py
+echo
 echo "Fetching Results:"
 python sw3_results_v1.py
 python sw3_results_v2.py
@@ -14,12 +20,6 @@ echo
 # echo "SUM Digits Pattern V2.1:"
 # python sum_digits_v2.1.py
 # echo
-echo "SYNC Digits Pattern V2.1:"
-python sync_digits_v2.1.py
-echo
-echo "SYNC Digits Pattern V2.2:"
-python sync_digits_v2.2.py
-echo
 # echo "SYNC Digits Pattern V1.1:"
 # python sync_digits_v1.1.py
 # echo
@@ -32,6 +32,12 @@ echo
 echo "PROCESSING STL:"
 cd /sdcard/Download/gidapp/stl
 echo
+echo "SYNC Digits Pattern V2.1:"
+python sync_digits_v2.1.py
+echo
+echo "SYNC Digits Pattern V2.2:"
+python sync_digits_v2.2.py
+echo
 echo "Fetching Results:"
 python stl_results_v1.py
 python stl_results_v2.py
@@ -42,19 +48,11 @@ echo
 # echo "SUM Digits Pattern V2.1:"
 # python sum_digits_v2.1.py
 # echo
-echo "SYNC Digits Pattern V2.1:"
-python sync_digits_v2.1.py
-echo
-echo "SYNC Digits Pattern V2.2:"
-python sync_digits_v2.2.py
-echo
 # echo "SYNC Digits Pattern V1.1:"
 # python sync_digits_v1.1.py
 # echo
 #echo "MISSING Digits Pattern V2.1:"
 #python missing_digits_v2.1.py
-echo
-echo
 echo
 echo
 #am start -a android.intent.action.VIEW -d file://~/sdcard/Download/gidapp/swertres/results_v2.txt -t text/plain --activity-clear-task
