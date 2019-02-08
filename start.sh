3d030ff8f1f7f97ae6b2f00bf78a9f1bf1fd2814
jsuci <jsuci.jsuci@gmail.com>
2019-02-08 20:44:19 +0800

    add remove and renamed entries

--- start.sh
+++ start.sh
@@ -4,18 +4,20 @@ clear
 echo "PROCESSING SW3:"
 cd /sdcard/Download/gidapp/swertres
 echo
-echo "SYNC Digits Pattern V2.4:"
-python sync_digits_v2.4.py
-echo
-echo "SOLID Pattern V1.2:"
-python solid_pattern_v1.2.py
+echo "DIFF_ZERO Pattern V1.1"
+python script_diff_zero_v1.1.py
+# echo "SYNC Digits Pattern V2.4:"
+# python script_sync_digits_v2.4.py
+# echo
+# echo "SOLID Pattern V1.2:"
+# python script_solid_pattern_v1.2.py
 echo
 echo "Fetching Results:"
 python sw3_results_v1.py
 python sw3_results_v2.py
 echo
 echo "Current Results:"
-python current_results.py
+python script_current_results.py
 echo
 # echo "SUM Digits Pattern V2.1:"
 # python sum_digits_v2.1.py
@@ -32,18 +34,20 @@ echo
 echo "PROCESSING STL:"
 cd /sdcard/Download/gidapp/stl
 echo
-echo "SYNC Digits Pattern V2.4:"
-python sync_digits_v2.4.py
-echo
-echo "SOLID Pattern V1.2:"
-python solid_pattern_v1.2.py
+echo "DIFF_ZERO Pattern V1.1"
+python script_diff_zero_v1.1.py
+# echo "SYNC Digits Pattern V2.4:"
+# python script_sync_digits_v2.4.py
+# echo
+# echo "SOLID Pattern V1.2:"
+# python script_solid_pattern_v1.2.py
 echo
 echo "Fetching Results:"
 python stl_results_v1.py
 python stl_results_v2.py
 echo
 echo "Current Results:"
-python current_results.py
+python script_current_results.py
 echo
 # echo "SUM Digits Pattern V2.1:"
 # python sum_digits_v2.1.py
@@ -56,8 +60,9 @@ echo
 echo
 echo
 #am start -a android.intent.action.VIEW -d file://~/sdcard/Download/gidapp/swertres/results_v2.txt -t text/plain --activity-clear-task
-read -p "Do you want to exit? (Y/y): " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	killall com.termux
-fi
+# read -p "Do you want to exit? (Y/y): " -n 1 -r
+# if [[ $REPLY =~ ^[Yy]$ ]]
+# then
+# 	killall com.termux
+# fi
+yes | killall com.termux
