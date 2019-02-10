e7d890518d2e2933b66f944ee957b4c94f243d3a
jsuci <jsuci.jsuci@gmail.com>
2019-02-10 18:54:42 +0800

    fix error in fetching date

--- stl/stl_results_v1.py
+++ stl/stl_results_v1.py
@@ -86,7 +86,10 @@ def get_results(file_month, file_date, file_year):
     entries = soup.find_all("div", class_="result")
 
     for i, e in enumerate(entries):
+        # Sunday, 10 February 2019
         web_date_list = e.h4.time.get_text().split(" ")
+
+        # date, day, month, year
         web_date = "{:02} {} {} {}".format(
             int(web_date_list[1]),
             web_date_list[0][:3].lower(),
