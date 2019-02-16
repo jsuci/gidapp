7bf66b740184a42d903a0acdde9b29bd693ca750
jsuci <jsuci.jsuci@gmail.com>
2019-02-17 01:59:10 +0800

    update results feb 17 2019

---
+++ start.bat
@@ -0,0 +1,7 @@
+cd "C:\Users\0x1\Desktop\gidapp\swertres"
+python sw3_results_v1.py
+python sw3_results_v2.py
+
+cd "C:\Users\0x1\Desktop\gidapp\stl"
+python stl_results_v1.py
+python stl_results_v2.py
