b00679923b11592a508e401cd499d8ccba3cb45a
jsuci <jsuci.jsuci@gmail.com>
2019-10-21 07:42:00 +0800

    add python dependencies on reinstall scripts

--- reinstall.bat
+++ reinstall.bat
@@ -1,3 +1,5 @@
-pip install bs4
 pip install requests
+pip install bs4
 pip install openpyxl
+pip install flake8
+pip install autopep8
--- reinstall.sh
+++ reinstall.sh
@@ -6,3 +6,5 @@ pkg install git
 pip install requests
 pip install bs4
 pip install openpyxl
+pip install flake8
+pip install autopep8
