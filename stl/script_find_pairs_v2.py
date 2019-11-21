147dc2f6c41a35fe9b64a6947ea1d6efffd56182
jsuci <jsuci.jsuci@gmail.com>
2019-11-22 01:44:47 +0800

    add option to filter base on ordered pairs

--- stl/script_find_pairs_v2.py
+++ stl/script_find_pairs_v2.py
@@ -17,10 +17,11 @@ def all_results():
 def match_results(num_one, num_two):
     output = []
 
-    for digit in num_one:
-        if digit in num_two:
-            output.append(digit)
-            num_two = num_two.replace(digit, "", 1)
+    if len(num_one) == 3:
+        for digit in num_one:
+            if digit in num_two:
+                output.append(digit)
+                num_two = num_two.replace(digit, "", 1)
 
     len_out = len(output)
 
@@ -53,34 +54,82 @@ def process_filter(filtered_results, num_two, fi):
     return [sorted(common_results), sorted(list(common_pairs))]
 
 
+def ordered_pairs(filtered_results, num_before, num_after, num_two):
+    results_only = []
+
+    for entry in filtered_results:
+        results_only.extend(entry[1:])
+
+    for i in range(len(results_only) - 1):
+        match_num_two = match_results(results_only[i], num_two)
+        if match_num_two and (len(match_num_two) == 3):
+            match_num_before = match_results(results_only[i - 1], num_before)
+            match_num_after = match_results(results_only[i - 1], num_after)
+
+            if match_num_before and len(match_num_before) >= 2:
+                return filtered_results
+            elif match_num_after and len(match_num_after) >= 2:
+                return filtered_results
+            else:
+                return None
+
+
 def filter_results():
 
-    num_two = input("Enter previous result: ")
+    num_two = input("Enter base result eg. 146, (510), 438: ")
+    num_before = input("Enter before base result eg. (146), 510, 438: ")
+    num_after = input("Enter after base result eg. 146, 510, (438): ")
     results = all_results()
     all_filter_results = []
     all_filter_pairs = []
+    all_ordered_pairs = []
 
     dic_all_results = {}
     dic_all_pairs = {}
 
-    with open("pairs_result_v2.txt", "w") as fi:
+    with open("find_pairs_v2.txt", "w") as fi:
+        print("matched results:")
+        fi.write("matched results:\n")
+
         for i in range(len(results) - 1):
             for entry in results[i]:
                 has_match = match_results(entry, num_two)
-                if has_match and (len(has_match) == 3) and (len(entry) == 3):
+                if has_match and (len(has_match) == 3):
                     filtered_results = [results[i - 1],
                                         results[i], results[i + 1]]
+
+                    ordered_pairs_results = ordered_pairs(
+                        filtered_results, num_before, num_after, num_two)
+
                     combine_results = process_filter(
                         filtered_results, num_two, fi)
 
                     all_filter_results.extend(combine_results[0])
                     all_filter_pairs.extend(combine_results[1])
 
+                    if ordered_pairs_results:
+                        all_ordered_pairs.extend(ordered_pairs_results)
+                        all_ordered_pairs.append("\n")
+
         for e in all_filter_results:
             dic_all_results[e] = dic_all_results.get(e, 0) + 1
 
         for e in all_filter_pairs:
             dic_all_pairs[e] = dic_all_pairs.get(e, 0) + 1
+
+        print(f"ordered pairs:")
+        fi.write(f"ordered pairs:\n")
+
+        for e in all_ordered_pairs:
+            if e == "\n":
+                print(f"\n\n")
+                fi.write(f"\n\n")
+            else:
+                print(f"{e}")
+                fi.write(f"{e}\n")
+
+        print(f"common results and pairs:")
+        fi.write(f"common results and pairs:\n")
 
         fi.write(
             f"{sorted(dic_all_results.items(), key=lambda x: x[1])[-5:]}\n")
