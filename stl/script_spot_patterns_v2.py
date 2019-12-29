1ecd693647a97f8fd2ebc0d70d1b004cf6e316b0
jsuc <jsuc.jsuci@gmail.com>
2019-12-29 12:57:30 -0500

    update results; set spot patterns v2 to filter atleast 1 common combi

--- stl/script_spot_patterns_v2.py
+++ stl/script_spot_patterns_v2.py
@@ -14,10 +14,9 @@ def read_results():
     return output
 
 
-def gap_results():
+def gap_results(sample_limit):
     all_results = read_results()
     output = {}
-    sample_limit = 3
     gap_limit = 10
 
     for gap in range(1, gap_limit + 1):
@@ -35,7 +34,7 @@ def gap_results():
     return output
 
 
-def filter_gaps():
+def filter_gaps(sample_limit):
     def check_common(results):
         common_digits = []
 
@@ -52,78 +51,46 @@ def filter_gaps():
 
         return sorted(common_digits)
 
-    dic_gap_results = gap_results()
+    dic_gap_results = gap_results(sample_limit)
     control = False
 
-    with open("spot_patterns_v2.txt", "w") as fo:
+    with open("spot_patterns_v1.txt", "a") as fo:
 
         for gap, results in dic_gap_results.items():
+
             common_digits = check_common(results)
 
-            if len(common_digits) == 1:
-                common_pair = check_common([results[0], results[-1]])
+            if len(common_digits) >= 1:
+                print(f"gap: {gap}, common: {common_digits}")
+                fo.write(f"gap: {gap}, common: {common_digits}\n")
 
-                if len(common_pair) == 2:
-                    print(f"gap: {gap:<5} common: {common_digits[0]}")
+                for result in reversed(results):
+                    temp_res = result
 
-                    for res in results:
-                        print(res)
+                    for common in common_digits:
+                        temp_res = temp_res.replace(common, "", 1)
 
-                    print(f"\n")
+                    print(f"{result}")
+                    fo.write(f"{result}\n")
 
-    #         if len(check_common(results)) == 1:
-    #             print(f"gap: {gap}")
-    #             fo.write(f"gap: {gap}\n")
+                print("\n")
+                fo.write("\n\n")
 
-    #             common_digits = check_common(results)
+                control = True
 
-    #             for result in reversed(results):
-    #                 temp_res = result
-
-    #                 for common in common_digits:
-    #                     temp_res = temp_res.replace(common, "", 1)
-
-    #                 if len(common_digits) == 1:
-    #                     print(
-    #                         f"{result} - [{temp_res[0]}] "
-    #                         f"[{common_digits[0]:<3}{common_digits[1]}]"
-    #                     )
-
-    #                     fo.write(
-    #                         f"{result} - [{temp_res[0]}] "
-    #                         f"[{common_digits[0]:<3}{common_digits[1]}]\n"
-    #                     )
-    #                 elif len(common_digits) == 1:
-    #                     print(
-    #                         f"{result} - [{temp_res[0]:<3}{temp_res[1]}] "
-    #                         f"[{common_digits[0]}]"
-    #                     )
-
-    #                     fo.write(
-    #                         f"{result} - [{temp_res[0]:<3}{temp_res[1]}] "
-    #                         f"[{common_digits[0]}]\n"
-    #                     )
-    #                 else:
-    #                     print(
-    #                         f"{result} - [{common_digits[0]:<3}"
-    #                         f"{common_digits[1]:<3}{common_digits[2]}]"
-    #                     )
-
-    #                     fo.write(
-    #                         f"{result} - [{common_digits[0]:<3}"
-    #                         f"{common_digits[1]:<3}{common_digits[2]}]"
-    #                     )
-
-    #             print("\n")
-    #             fo.write("\n\n")
-
-    #             control = True
-
-    # return control
+    return control
 
 
 def main():
-    filter_gaps()
+    with open("spot_patterns_v1.txt", "w") as fi:
+        fi.write("")
+
+    sample_limit = 3
+    while True:
+        if filter_gaps(sample_limit):
+            sample_limit += 1
+        else:
+            break
 
 
 if __name__ == "__main__":
