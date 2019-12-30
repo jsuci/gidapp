c45bd7005ad38a4666cd48775aee323ff2b53cf5
jsuci <jsuci.jsuci@gmail.com>
2019-12-31 07:03:56 +0800

    fix startup script; update results

--- win10_start.bat
+++ win10_start.bat
@@ -1,23 +1,23 @@
-cd "C:\Users\0x1\Desktop\Repo\gidapp\swertres"
+cd "D:\Repo\gidapp\swertres"
 python sw3_results_v1.py
 python sw3_results_v2.py
 
-cd "C:\Users\0x1\Desktop\Repo\gidapp\stl"
+cd "D:\Repo\gidapp\stl"
 python stl_results_v1.py
 python stl_results_v2.py
 
-cd "C:\Users\0x1\Desktop\Repo\gidapp\swertres"
+cd "D:\Repo\gidapp\swertres"
 python script_current_results.py
 
-cd "C:\Users\0x1\Desktop\Repo\gidapp\stl"
+cd "D:\Repo\gidapp\stl"
 python script_current_results.py
 
-cd "C:\Users\0x1\Desktop\Repo\gidapp\swertres"
+cd "D:\Repo\gidapp\swertres"
 python script_spot_patterns_v1.py
 python script_spot_patterns_v2.py
 python script_common_combi_pair.py
 
-cd "C:\Users\0x1\Desktop\Repo\gidapp\stl"
+cd "D:\Repo\gidapp\stl"
 python script_spot_patterns_v1.py
 python script_spot_patterns_v2.py
 python script_common_combi_pair.py
