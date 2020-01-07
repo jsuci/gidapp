b36c42af5913cdf483688d0409a154593695f3e6
jsuci <jsuci.jsuci@gmail.com>
2020-01-08 07:21:14 +0800

    fix_date_results progress; update results

--- swertres/fix_date_results.py
+++ swertres/fix_date_results.py
@@ -1,8 +1,8 @@
-from datetime import datetime, timedelta
-from itertools import islice
+from itertools import islice, chain
 from re import split
 from openpyxl import Workbook
 from openpyxl.styles import (NamedStyle, Alignment, Font)
+from datetime import datetime
 
 
 def read_results():
@@ -14,6 +14,12 @@ def read_results():
 
         for entry in islice(fi, 1, None):
             entry = split(r"\s{2,}", entry.strip())
+            entry[0] = datetime.strptime(
+                entry[0],
+                "%d %a %b %Y"
+            ).strftime(
+                "%d %B %Y"
+            )
 
             output.append(entry)
 
@@ -23,56 +29,56 @@ def read_results():
             return output[:-1]
 
 
-# def fix_results():
-#     all_results = read_results()
-#     output = []
-
-#     for i in range(len(all_results) - 1):
-#         first_date = datetime.strptime(all_results[i][0], "%d %B %Y")
-#         second_date = datetime.strptime(all_results[i + 1][0], "%d %B %Y")
-#         diff_date = second_date - first_date
-#         diff_month = second_date.month - first_date.month
-#         diff_year = second_date.year - first_date.year
-
-#         if diff_month > 0:
-#             output.append(all_results[i])
-#             output.append("end_month")
-#         elif diff_date.days > 0:
-#             output.append(all_results[i])
-#             for i in range(1, diff_date.days):
-#                 time_fill_obj = first_date + timedelta(days=i)
-#                 time_fill_str = time_fill_obj.strftime("%d %B %Y")
-
-#                 if time_fill_obj.day == 1 and time_fill_obj.month == 1:
-#                     output.append("end_year"),
-#                     output.append([time_fill_str, "", "", ""])
-#                 else:
-#                     output.append([time_fill_str, "", "", ""])
-#         else:
-#             output.append(all_results[i])
-
-#     output.append(all_results[-1])
-
-#     return output
-
 def fix_results():
     all_results = read_results()
+    month_results = []
+    temp_results = []
     output = []
-    temp_output = []
 
-    for i in range(len(all_results) - 1):
-        first_date = datetime.strptime(all_results[i][0], "%d %a %b %Y")
-        second_date = datetime.strptime(all_results[i + 1][0], "%d %a %b %Y")
-        diff_month = second_date.month - first_date.month
+    for i in range(1, len(all_results)):
+        # pev_month = January
+        prev_month = all_results[i - 1][0].split(" ")[1]
+        next_month = all_results[i][0].split(" ")[1]
 
-        if diff_month != 0 or diff_month == -11:
-            temp_output.append(all_results[i])
-            output.append(temp_output)
-            temp_output = []
+        if prev_month == next_month:
+            temp_results.append(all_results[i - 1])
         else:
-            temp_output.append(all_results[i])
+            temp_results.append(all_results[i - 1])
+            month_results.append(temp_results)
+            temp_results = []
 
-    return output
+    # Append the last entry
+    temp_results.append(all_results[-1])
+    month_results.append(temp_results)
+
+    for month_res in month_results:
+        for i in range(32 - 1):
+            date_one = int(f"{i + 1:02d}")
+            add_entry = [
+                f"{date_one:02d} {month_res[0][0][3:]}",
+                "",
+                "",
+                ""
+            ]
+
+            try:
+                month_res_index = month_res.index(month_res[i])
+                date_two = int(month_res[i][0][:2])
+            except Exception:
+                month_res.append(add_entry)
+                continue
+
+            if date_one != date_two:
+                date_one_int = i
+                date_two_int = int(month_res[i][0][:2])
+
+                if date_one_int < date_two_int:
+                    month_res.insert(month_res_index, add_entry)
+
+        output.append(month_res)
+
+    for e in list(chain(*output)):
+        print(e)
 
 
 def excel_export():
@@ -165,8 +171,7 @@ def excel_export():
 
 def main():
     # excel_export()
-    for res in fix_results():
-        print(len(res), res[-1])
+    fix_results()
 
 
 if __name__ == "__main__":
