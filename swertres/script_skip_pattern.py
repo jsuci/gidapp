7c48ee0e00a0d6edbd3a8b7413d067849a5063bf
jsuci <jsuci.jsuci@gmail.com>
2021-05-17 00:39:14 +0800

    improve output for skip_pattern script

--- swertres/script_skip_pattern.py
+++ swertres/script_skip_pattern.py
@@ -1,31 +1,49 @@
 from re import split
+from pathlib import Path
 
 
 def skip_pattern(user_num):
     output = {}
 
     for i in range(0, 10):
-        output.setdefault(str(i), [])
+        output.setdefault(str(i), '')
 
     for dg in user_num[0]:
-        if len(output[dg]) == 0:
-            output[dg].append('current')
+        if output[dg] == '':
+            output[dg] = 'current'
 
     for skip, res in enumerate(user_num, start=1):
         for dg in res:
-            if len(output[dg]) == 0:
-                output[dg].append(skip - 1)
+            if output[dg] == '':
+                output[dg] = skip - 1
 
     return output
 
 
 def main():
-    user_num = split(r"\s{1,}", input("Enter at least 9 results: ").strip())
+    contents = []
+    fpath = Path("previous_9_results.txt")
 
-    result = skip_pattern(user_num)
+    with open(fpath, "r") as f:
+        for line in f:
+            line = split(r"\s{1,}", line.strip())
+            contents.extend(line)
+
+    result = skip_pattern(contents)
+
+    highest_count = 0
+    highest = ''
+    empty = []
 
     for k, v in result.items():
-        print(k, v)
+        if v == '':
+            empty.append(k)
+        else:
+            if v != 'current' and highest_count < v:
+                highest_count = v
+                highest = k
+
+    print(f"highest: {highest}\nempty: {empty}")
 
 
 if __name__ == "__main__":
