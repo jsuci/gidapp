bcd960671929a94ffaf29c17ab8f9567c2f9c4d8
jsuci <jsuci.jsuci@gmail.com>
2021-05-30 03:52:04 +0800

    remove previous excel script

--- gitbash_start.sh
+++ gitbash_start.sh
@@ -8,9 +8,9 @@ python script_digit_position.py
 python script_digit_pair.py
 python script_digit_gap_pair.py
 
-python script_excel_results.py
 python script_excel_results_2.py
 python script_excel_results_3.py
+python script_excel_results_4.py
 
 python script_odd_even_big_small.py
 python script_duplicate_pairs.py
